bf6e7af212287a82d8f7435f317f3609715998d8
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-18 15:31:16 +0600

    Home: updated home page with successfully loaded all brands with brand image and name

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,10 @@
+import Brands from "../../Components/Brands/Brands";
+
 const Home = () => {
     return (
         <div>
             <h2>Home</h2>
+            <Brands></Brands>
         </div>
     );
 };
