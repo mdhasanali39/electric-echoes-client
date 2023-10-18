232acaf23d7403b19962b9793d3b5425c827d89c
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-18 08:13:17 +0600

    Routes: Routes.jsx updated with routes for add product, my cart, login and register pages

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home/Home";
+import AddProduct from "../Pages/AddProduct/AddProduct";
+import MyCart from "../Pages/MyCart/MyCart";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +15,22 @@ const router = createBrowserRouter([
                 path: '/',
                 element: <Home></Home>
             },
+            {
+                path: '/add_product',
+                element: <AddProduct></AddProduct>
+            },
+            {
+                path: '/my_cart',
+                element: <MyCart></MyCart>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
         ]
     }
 ])
