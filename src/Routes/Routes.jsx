23c043a5390acd53971b212cf00d0b5e2c4c8a82
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-21 22:14:21 +0600

    Routes: this Routes component have all the routes in this web app

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,11 +5,17 @@ import AddProduct from "../Pages/AddProduct/AddProduct";
 import MyCart from "../Pages/MyCart/MyCart";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import Products from "../Pages/Products/Products";
+import UpdateProduct from "../Pages/UpdateProduct/UpdateProduct";
+import ProductDetails from "../Pages/ProductDetails/ProductDetails";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
@@ -17,11 +23,30 @@ const router = createBrowserRouter([
             },
             {
                 path: '/add_product',
-                element: <AddProduct></AddProduct>
+                element: <PrivateRoute>
+                    <AddProduct></AddProduct>
+                </PrivateRoute>
+            },
+            {
+                path: '/update_products/:id',
+                element: <PrivateRoute>
+                    <UpdateProduct></UpdateProduct>
+                </PrivateRoute>,
+                loader: ({params})=> fetch(`https://electric-echoes-server.vercel.app/electricechoes/product/${params.id}`)
+            },
+            {
+                path: 'product_details/:id',
+                element: <PrivateRoute>
+                    <ProductDetails></ProductDetails>
+                </PrivateRoute>,
+                loader: ({params})=> fetch(`https://electric-echoes-server.vercel.app/electricechoes/product/${params.id}`)
             },
             {
                 path: '/my_cart',
-                element: <MyCart></MyCart>
+                element: <PrivateRoute>
+                    <MyCart></MyCart>
+                </PrivateRoute>,
+                loader: ()=> fetch('https://electric-echoes-server.vercel.app/electricechoes/carts')
             },
             {
                 path: '/login',
@@ -30,6 +55,11 @@ const router = createBrowserRouter([
             {
                 path: '/register',
                 element: <Register></Register>
+            },
+            {
+                path: '/products/:brand',
+                element: <Products></Products>,
+                loader: ({params})=> fetch(`https://electric-echoes-server.vercel.app/electricechoes/products/${params.brand}`)
             }
         ]
     }
