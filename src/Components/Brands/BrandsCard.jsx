3c1898a1d31601eb546e040e94f97a9a6d9063d2
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-21 21:46:52 +0600

    brandsCard: in this component I have created a layout for how each and every brands look like

--- src/Components/Brands/BrandsCard.jsx
+++ src/Components/Brands/BrandsCard.jsx
@@ -1,9 +1,21 @@
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 const BrandsCard = ({ brand }) => {
+
+  const navigate = useNavigate()
+
   const { name, photoUrl } = brand || {};
+
+
+  const handleBrand = () =>{
+    navigate(`/products/${brand.name}`)
+  }
+
   return (
-    <div className="relative flex w-full  flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+    <div
+    onClick={handleBrand}
+     className="relative flex w-full  flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
       <div className="relative overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
         <img src={photoUrl} alt={name} />
         {/* <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-tr from-transparent via-transparent to-black/60"></div> */}
