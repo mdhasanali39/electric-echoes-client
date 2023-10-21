a6225a1b973faae435a9bd7da5ffeb76e3de76f5
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-21 22:09:27 +0600

    MainLayout: this is the root layout of this web app and this layout have wraped all components and pages

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,13 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Components/Navbar/Navbar';
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="font-Roboto">
+      <Navbar></Navbar>
+      <div className="max-w-[1300px] mx-auto px-4 py-5">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+      <Toaster/>
+    </div>
+  );
 };
 
 export default MainLayout;
